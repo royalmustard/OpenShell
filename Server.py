7d431d7a6e669601fbb3e1cecf8402c7e2e55576
royalmustard <36129876+royalmustard@users.noreply.github.com>
2019-10-06 09:20:46 +0200

    first test

--- Server.py
+++ Server.py
@@ -1,1 +1,15 @@
+import socket
 
+IP = "127.0.0.1"
+PORT = 42069
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((IP, PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(data)
--- client.lua
+++ client.lua
@@ -1,1 +1,5 @@
-//Test
+local internet = require("internet")
+local handle = internet.open("example.com", 1337)
+
+handle:write("1234")
+handle:close()
