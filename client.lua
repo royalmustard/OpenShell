bddd8a6357b3d6d84f0387e8df7c1fa83e161a36
royalmustard <Felix@csruediger.de>
2019-10-16 19:40:30 +0200

    Moced error to MemiumError basic input loop for client

--- client.lua
+++ client.lua
@@ -3,5 +3,12 @@ print("got require")
 local handle = internet.open("127.0.0.1", 42069)
 print("got handle")
 handle.stream.socket.finishConnect()
-print(handle:write("1234"))
+lines = {}
+str = io.read()
+while str do
+    handle:write(str)
+    data = handle.read(1024)
+    print(data)
+    str = io.read()
+end
 handle:close()
